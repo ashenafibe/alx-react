26d9591774794b957979969ee937cc7f1964d388
Ashenafibe <Abekele119@gmail.com>
2023-08-03 11:33:47 +0300

    hello

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,46 +1,50 @@
-import React, { Component, Fragment } from 'react';
-import Header from '../Header/Header';
+import React from 'react';
+/* Components */
 import Login from '../Login/Login';
+import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
-import PropTypes from 'prop-types';
+import Notifications from '../Notifications/Notifications';
 import { getLatestNotification } from '../utils/utils';
+/* Proptypes */
+import { bool } from 'prop-types';
+/* Styles */
+import './App.css';
 
-const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
-const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-];
+function App({isLoggedIn}) {
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { isLoggedIn } = this.props;
-    return (
-      <Fragment>
-        <Notifications listNotifications={listNotifications} />
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
+  ];
+
+  return (
+    <>
+      <Notifications listNotifications={listNotifications} />
+      <div className="App">
+        {/* Header */}
         <Header />
+        {/* Body */}
         {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        {/* Footer */}
         <Footer />
-      </Fragment>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
-App.defaultProps = {
-  isLoggedIn: false,
+App.propTypes = {
+  isLoggedIn: bool,
 };
 
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
+App.defaultProps = {
+  isLoggedIn: true,
 };
 
 export default App;
